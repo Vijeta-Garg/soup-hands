59194035d0e9e0682212efee7c71a7fa9be4efa8
Vijeta-Garg <121990080+Vijeta-Garg@users.noreply.github.com>
2026-07-06 06:18:52 -0400

    Update MOTORSWORKSLOW.ino

--- MOTORSWORKSLOW.ino
+++ MOTORSWORKSLOW.ino
@@ -1,10 +1,14 @@
+
+
 const int AIN1 = 4;
 const int AIN2 = 5;
 
-const int BIN1 = 6;
-const int BIN2 = 7;
+const int BIN1 = 2;
+const int BIN2 = 3;
 
 void setup() {
+  Serial.begin(115200);
+
   pinMode(AIN2, OUTPUT);
   pinMode(BIN2, OUTPUT);
 
@@ -16,6 +20,29 @@ void setup() {
 }
 
 void loop() {
-  ledcWrite(AIN1, 45);   // Motor A speed
-  ledcWrite(BIN1, 45);   // Motor B speed
+  if(Serial.available() > 0){
+    incomingByte = Serial.read();
+
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+  }
+
+  // Motor A
+   ledcWrite(AIN1, 90);
+   ledcWrite(BIN1, 0);
+  delay(1000);
+  // Stop both
+ ledcWrite(AIN1, 0);
+ ledcWrite(BIN1, 0);
+ delay(250);
+
+  // // Motor B
+ ledcWrite(AIN1, 0);
+ ledcWrite(BIN1, 90);
+ delay(1000);
+
+  // // Stop both
+ ledcWrite(AIN1, 0);
+ ledcWrite(BIN1, 0);
+ delay(250);
 }
