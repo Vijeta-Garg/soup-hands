9a40bc202b7e3f0fef2f331405a0f44cc94a0a1c
Vijeta-Garg <121990080+Vijeta-Garg@users.noreply.github.com>
2026-07-06 11:35:05 -0400

    Refactor motor pins and implement ESP-NOW
    Updated motor control pins and added ESP-NOW functionality.

--- MOTORSWORKSLOW.ino
+++ MOTORSWORKSLOW.ino
@@ -1,13 +1,50 @@
+#include <WiFi.h>
+#include <esp_now.h>
 
-
-const int AIN1 = 4;
-const int AIN2 = 5;
+const int AIN1 = 6;
+const int AIN2 = 7;
 
 const int BIN1 = 2;
 const int BIN2 = 3;
 
+typedef struct {
+    uint8_t sensorNumber;
+    float rawData;
+    uint8_t pressureCategory;
+} struct_message;
+
+struct_message incomingData;
+
+void onReceive(const esp_now_recv_info_t *info,
+               const uint8_t *data,
+               int len)
+{
+    if (len == sizeof(incomingData)) {
+        memcpy(&incomingData, data, sizeof(incomingData));
+
+        Serial.print(incomingData.sensorNumber);
+        Serial.print(",");
+        Serial.print(incomingData.rawData, 2);
+        Serial.print(",");
+        Serial.println(incomingData.pressureCategory);
+    }
+}
+
 void setup() {
   Serial.begin(115200);
+  // delay(1000);
+
+
+  Serial.println("hello!");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect(); 
+  delay(100);
+
+  Serial.println(WiFi.macAddress());
+
+  esp_now_init();
+  esp_now_register_recv_cb(onReceive); 
 
   pinMode(AIN2, OUTPUT);
   pinMode(BIN2, OUTPUT);
@@ -20,29 +57,30 @@ void setup() {
 }
 
 void loop() {
-  if(Serial.available() > 0){
-    incomingByte = Serial.read();
+//   if(Serial.available() > 0){
+//      incomingByte = Serial.read();
 
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
-  }
+//     Serial.print("I received: ");
+//     Serial.println(incomingByte, DEC);
 
-  // Motor A
-   ledcWrite(AIN1, 90);
-   ledcWrite(BIN1, 0);
-  delay(1000);
-  // Stop both
- ledcWrite(AIN1, 0);
- ledcWrite(BIN1, 0);
- delay(250);
+// }
 
-  // // Motor B
- ledcWrite(AIN1, 0);
- ledcWrite(BIN1, 90);
- delay(1000);
+   // Motor A
+    ledcWrite(AIN1, 90);
+    ledcWrite(BIN1, 0);
+   delay(1000);
+//   // Stop both
+  ledcWrite(AIN1, 0);
+  ledcWrite(BIN1, 0);
+  delay(250);
 
-  // // Stop both
- ledcWrite(AIN1, 0);
- ledcWrite(BIN1, 0);
- delay(250);
+//   // // Motor B
+  ledcWrite(AIN1, 0);
+  ledcWrite(BIN1, 90);
+delay(1000);
+
+   // // Stop both
+  ledcWrite(AIN1, 0);
+  ledcWrite(BIN1, 0);
+  delay(250);
 }
